845e81b1c6980e6cb05e2f578f9b51e0b64465d0
Tindur20 <sigsteinn@gmail.com>
2017-04-06 10:59:46 +0000

    lagaði útlitið á síðuni (Ekki tilbúið)

--- scripts.js
+++ scripts.js
@@ -11,20 +11,21 @@ $.ajax({
 });
 
 var el_ul = document.getElementById("container");
-function show(data){
-          for (var i = 0; i < data.results.length; i++) {
-            //bý hér til li tög til að láta allt fyrir neðan í til að vinna með það.
-            var el_li = document.createElement("li");
-            //bý hér til div html tag til að láta það sem er um myndina/tónleikana.
-            var el_title = document.createElement("div");
-            var el_skrifa = document.createTextNode(data.results[i].eventDateName);
-            el_title.appendChild(el_skrifa);
-            //bý til img html tagið hér og læt myndirnar í það.
-            var el_mynd = document.createElement("img");
-            el_mynd.src = data.results[i].imageSource;
-            el_li.appendChild(el_mynd);
-            el_li.appendChild(el_title);
-            el_ul.appendChild(el_li);
-
-        }     
+  function show(data){
+    for (var i = 0; i < data.results.length; i++) {
+    //bý hér til li tög til að láta allt fyrir neðan í til að vinna með það.
+    var el_li = document.createElement("li");
+    el_li.className = 'element';
+    //bý hér til div html tag til að láta það sem er um myndina/tónleikana.
+    var el_title = document.createElement("div");
+    el_title.className = 'event';
+    var el_skrifa = document.createTextNode(data.results[i].eventDateName);
+    el_title.appendChild(el_skrifa);
+    //bý til img html tagið hér og læt myndirnar í það.
+    var el_mynd = document.createElement("img");
+    el_mynd.src = data.results[i].imageSource;
+    el_li.appendChild(el_mynd);
+    el_li.appendChild(el_title);
+    el_ul.appendChild(el_li);
+  }     
 }
