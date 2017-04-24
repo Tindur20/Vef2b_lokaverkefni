e800462869951af08bbde1de2b746442098cd575
Tindur20 <sigsteinn@gmail.com>
2017-04-24 16:18:49 +0000

    brja að láta tag filter (virkar ekki allveg)

--- scripts.js
+++ scripts.js
@@ -1,31 +1,90 @@
 "use strict";
+
 $.ajax({
   'url': 'http://apis.is/concerts',
   'type': 'GET',
   'dataType': 'json',
   'success': function(data) {
     console.log(data);
-
+    //sýnir upplýsingar
     show(data);
+
+
+
+  var $imgs = $('#container img');                // Store all images
+  var $buttons = $('#buttons');                   // Store buttons element
+  var tagged = {};                                // Create tagged object
+
+  $imgs.each(function() {                         // Loop through images and
+    var img = this;                               // Store img in variable
+    var tags = $(this).data('tags');              // Get this element's tags
+
+    if (tags) {                                   // If the element had tags
+      tags.split(',').forEach(function(tagName) { // Split at comma and
+        if (tagged[tagName] == null) {            // If object doesn't have tag
+          tagged[tagName] = [];                   // Add empty array to object
+        }
+        tagged[tagName].push(img);                // Add the image to the array
+      });
+    }
+  });
+
+  $('<button/>', {                                 // Create empty button
+    text: 'Show All',                              // Add text 'show all'
+    class: 'active',                               // Make it active
+    click: function() {                            // Add onclick handler to
+      $(this)                                      // Get the clicked on button
+        .addClass('active')                        // Add the class of active
+        .siblings()                                // Get its siblings
+        .removeClass('active');                    // Remove active from siblings
+      $imgs.show();                                // Show all images
+    }
+  }).appendTo($buttons);                           // Add to buttons
+
+  $.each(tagged, function(tagName) {               // For each tag name
+    $('<button/>', {                               // Create empty button
+      text: tagName + ' (' + tagged[tagName].length + ')', // Add tag name
+      click: function() {                          // Add click handler
+        $(this)                                    // The button clicked on
+          .addClass('active')                      // Make clicked item active
+          .siblings()                              // Get its siblings
+          .removeClass('active');                  // Remove active from siblings
+        $imgs                                      // With all of the images
+          .hide()                                  // Hide them
+          .filter(tagged[tagName])                 // Find ones with this tag
+          .show();                                 // Show just those images
+      }
+    }).appendTo($buttons);                         // Add to the buttons
+  });
+
+
+
   }
 });
 
 var el_ul = document.getElementById("container");
-  function show(data){
-    for (var i = 0; i < data.results.length; i++) {
+
+  function show(data){ 
+    var gogn = data.results;  
+    for (var i = 0; i < gogn.length; i++) {
     //bý hér til li tög til að láta allt fyrir neðan í til að vinna með það.
     var el_li = document.createElement("li");
     el_li.className = 'element';
     //bý hér til div html tag til að láta það sem er um myndina/tónleikana.
     var el_title = document.createElement("div");
     el_title.className = 'event';
-    var el_skrifa = document.createTextNode(data.results[i].eventDateName);
+    var el_skrifa = document.createTextNode(gogn[i].eventDateName);
     el_title.appendChild(el_skrifa);
     //bý til img html tagið hér og læt myndirnar í það.
     var el_mynd = document.createElement("img");
-    el_mynd.src = data.results[i].imageSource;
+    el_mynd.src = gogn[i].imageSource;
+    //bæta við data-tags
+    el_mynd.setAttribute('data-tags', gogn[i].eventHallName);
     el_li.appendChild(el_mynd);
     el_li.appendChild(el_title);
     el_ul.appendChild(el_li);
+
   }     
 }
+
+
